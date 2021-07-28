ef1c62bda73d7de004b205a355c1131f5894bc3f
Dinaisbaih <isbaihdina.f@outlook.com>
2021-07-28 16:45:43 +0300

    friendShip reation

---
+++ routes/friendShips.js
@@ -0,0 +1,18 @@
+//Setup
+const express = require("express");
+const router = express.Router();
+
+//Controllers
+const {
+  friendShipCreate,
+  friendShipList,
+} = require("../controllers/friendShipController");
+
+router.post(
+  "/",
+  // passport.authenticate("jwt", { session: false }),
+  friendShipCreate
+);
+
+router.get("/", friendShipList);
+module.exports = router;
